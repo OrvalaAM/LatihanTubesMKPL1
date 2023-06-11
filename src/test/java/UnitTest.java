431e1b3cf9053df936d01f25a451276ce53faa9f
OrvalaAM <90945844+OrvalaAM@users.noreply.github.com>
2023-06-11 20:07:55 +0700

    init commit

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.Test;
+import MainProgram.DBconn;
+import Model.Turnamen;
+import java.sql.*;
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void testConnection(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost/6pm", "root", "");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        assertEquals(con, DBconn.getConnection());
+    }
+    @Test
+    public void testNamaTurnamen(){
+        Turnamen turnamenTest = new Turnamen("Nama", "peserta");
+        assertEquals("Nama", turnamenTest.getNamaTurnamen());
+    }
+    @Test
+    public void testPesertaTurnamen(){
+        Turnamen turnamenTest = new Turnamen("Nama", "peserta");
+        assertEquals("peserta", turnamenTest.getPeserta());
+    }
+    
+}
